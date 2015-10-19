629e2de9ab2d9eb45b0156311e4dd320d300e643
Julio Carozo <jccarozo@gmail.com>
2015-10-19 10:24:14 -0700

    SEOInspector.js
    Javascript File

---
+++ SEOInspector.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    var homePage = "http://staging.hairgrowthcenters.com/";
+    var visitedPages = Array();
+    searchLinks(homePage);
+    function searchLinks( url ){
+        console.log(" Current Page: ", url);
+        if (visitedPages.indexOf(url)>=0){
+            return;
+        }else{
+            visitedPages.push(url);
+        }
+        $.get( url, function( data ) {
+            var hrefs = $(data).find("a");
+            var imgs = $(data).find("img");
+            console.log("  Images:")
+            for (var i=0; i<imgs.length;i++){
+                console.log("    ",$(imgs[i]).attr("src"));
+            }
+            console.log(" - - - - - - -  - - - - - ");
+            console.log("  Links:")
+            for (var i=0; i<hrefs.length;i++){
+                console.log("    href=",$(hrefs[i]).attr("href")," class=",$(hrefs[i]).attr("class")," id=",$(hrefs[i]).attr("id"));
+            }
+            for (var i=0; i<hrefs.length;i++){
+                if ($(hrefs[i]).attr("href").search(homePage)>=0){
+                    searchLinks($(hrefs[i]).attr("href") );
+                }
+            }
+        });
+    }
+});
